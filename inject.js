c0a0c18be4afbe000f6d1f2151c56a3305268730
Tom Milburn <thomasjosephmilburn@gmail.com>
2015-04-04 18:35:18 -0400

    added a nicer block page

--- inject.js
+++ inject.js
@@ -3,6 +3,6 @@ safari.self.addEventListener("message", blockPage, false);
 function blockPage(msgEvent) {
   console.log(msgEvent);
   if (msgEvent.name === "block") {
-    document.getElementsByTagName('body')[0].innerHTML = "<h1>You should be working!</h1>";
+    document.location = safari.extension.baseURI + 'blocked.html';
   }
 }
